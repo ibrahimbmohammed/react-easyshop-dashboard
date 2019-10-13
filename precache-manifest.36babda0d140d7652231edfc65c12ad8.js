600f652fcb152ead45801787a1ffe075535e6e1a
Mohammed Ibrahim B <ibrahimbmohammed@gmail.com>
2019-10-13 07:54:30 +0100

    Updates

---
+++ precache-manifest.36babda0d140d7652231edfc65c12ad8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aaf5078c3899593c9ecc4fda0151e70",
+    "url": "/ibrahimbmohammed/react-easyshop-dashboard/index.html"
+  },
+  {
+    "revision": "407be11e03c086f35248",
+    "url": "/ibrahimbmohammed/react-easyshop-dashboard/static/js/2.8219679c.chunk.js"
+  },
+  {
+    "revision": "df47d5000b7a3524699b",
+    "url": "/ibrahimbmohammed/react-easyshop-dashboard/static/js/main.3476b393.chunk.js"
+  },
+  {
+    "revision": "f6059328f2aee1b7d2d9",
+    "url": "/ibrahimbmohammed/react-easyshop-dashboard/static/js/runtime-main.c45e8629.js"
+  },
+  {
+    "revision": "0198055090faa12fa291aacdedffcff4",
+    "url": "/ibrahimbmohammed/react-easyshop-dashboard/static/media/IMG-20190919-WA0012.01980550.jpg"
+  },
+  {
+    "revision": "37f523bc0cafa6b1187ff22c2f3336bf",
+    "url": "/ibrahimbmohammed/react-easyshop-dashboard/static/media/IMG-20190919-WA0013.37f523bc.jpg"
+  },
+  {
+    "revision": "f88d7d0a1c4382890954e2e85a62e0a7",
+    "url": "/ibrahimbmohammed/react-easyshop-dashboard/static/media/IMG-20190919-WA0014.f88d7d0a.jpg"
+  },
+  {
+    "revision": "2a7486d664b3537bc6ae81ff9bdb0878",
+    "url": "/ibrahimbmohammed/react-easyshop-dashboard/static/media/thumb.2a7486d6.jpg"
+  }
+]);
